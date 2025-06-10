b549254d2e1c218fa36f0254736138aef84c5aeb
Yugal88943 <kashyapyugal17@gmail.com>
2025-06-10 14:18:09 +0530

    Build Completed

--- src/pages/Get-Involved/Partner-With-Us/Partner-With-Us-Components/Partner-With-Us-Component-Background.js
+++ src/pages/Get-Involved/Partner-With-Us/Partner-With-Us-Components/Partner-With-Us-Component-Background.js
@@ -1,4 +1,4 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import partnerWithUsVideo from '@/assets/Get-Involved-Section/join_hands_with_naipunayam_foundation.webm';
-const PartnerWithUsComponentBackgroundImage = () => (_jsx("section", { className: "w-full mt-[1px]", children: _jsx("video", { src: partnerWithUsVideo, autoPlay: true, muted: true, playsInline: true, className: "w-full aspect-[16/9] object-cover object-center", draggable: false }) }));
+const PartnerWithUsComponentBackgroundImage = () => (_jsx("section", { className: "w-full mt-[1px]", children: _jsx("video", { src: partnerWithUsVideo, autoPlay: true, muted: true, playsInline: true, className: "w-full aspect-[16/9] object-cover object-center", draggable: false, controls: false, disablePictureInPicture: true, controlsList: "nodownload nofullscreen noremoteplayback" }) }));
 export default PartnerWithUsComponentBackgroundImage;
--- src/pages/Our-Initiatives/Vihaas-Design-Technologies/Vihaas-Design-Technologies-Components/Vihaas-Design-Technologies-Component-Background.js
+++ src/pages/Our-Initiatives/Vihaas-Design-Technologies/Vihaas-Design-Technologies-Components/Vihaas-Design-Technologies-Component-Background.js
@@ -1,4 +1,4 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import vihaasDesignVideo from "@/assets/Our-Initiatives-Section/vihaas_design_video.webm";
-const VihaasDesignTechnologiesComponentBackground = () => (_jsx("section", { className: "w-full mt-[1px]", children: _jsx("video", { src: vihaasDesignVideo, autoPlay: true, muted: true, playsInline: true, className: "w-full aspect-[16/9] object-cover object-center", draggable: false }) }));
+const VihaasDesignTechnologiesComponentBackground = () => (_jsx("section", { className: "w-full mt-[1px]", children: _jsx("video", { src: vihaasDesignVideo, autoPlay: true, muted: true, playsInline: true, className: "w-full aspect-[16/9] object-cover object-center", draggable: false, controls: false, disablePictureInPicture: true, controlsList: "nodownload nofullscreen noremoteplayback" }) }));
 export default VihaasDesignTechnologiesComponentBackground;
